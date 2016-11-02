8d695074496cef4cb010814accaf0507233f629e
Cécilia Bossard <cecilia.bossard@gmail.com>
2016-11-02 15:11:18 +0100

    Init

---
+++ google-blockly/blocks/custom.js
@@ -0,0 +1,19 @@
+'use strict';
+
+goog.provide('Blockly.Blocks.texts');
+
+goog.require('Blockly.Blocks');
+
+Blockly.Blocks['base_delay'] = {
+  helpUrl: 'http://arduino.cc/en/Reference/delay',
+  init: function() {
+    this.setColour(120);
+    this.appendValueInput("DELAY_TIME", 'Number')
+        .appendField("Attendre")
+        .setCheck('Number');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('Delay specific time');
+  }
+};
---
+++ google-blockly/generators/arduino/custom.js
@@ -0,0 +1,17 @@
+Blockly.Arduino.base_delay = function() {
+  var delay_time = Blockly.Arduino.valueToCode(this, 'DELAY_TIME', Blockly.Arduino.ORDER_ATOMIC) || '1000'
+  var code = 'delay(' + delay_time + ');\n';
+  return code;
+};
+
+
+Blockly.Arduino['text_indexOf'] = function(block) {
+  // Search the text for a substring.
+  var operator = block.getFieldValue('END') == 'FIRST' ? 'indexOf' : 'lastIndexOf';
+  var subString = Blockly.Arduino.valueToCode(block, 'FIND',
+      Blockly.Arduino.ORDER_NONE) || '\'\'';
+  var text = Blockly.Arduino.valueToCode(block, 'VALUE',
+      Blockly.Arduino.ORDER_MEMBER) || '\'\'';
+  var code = text + '.' + operator + '(' + subString + ')';
+  return [code, Blockly.Arduino.ORDER_MEMBER];
+};
